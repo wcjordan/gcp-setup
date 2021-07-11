3ee393de5a893a7c2c96f2892bb96ffeaac7b679
Chris Jordan <flipperkid@gmail.com>
2021-07-11 09:13:27 -0400

    Setup Config Connector w/ Terraform (#1)
    Use Helm to install Config Connector
    Update README with installation instructions

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # gcp-setup
 Prerequisites: Install Terraform & [Set up GCP](https://learn.hashicorp.com/tutorials/terraform/google-cloud-platform-build?in=terraform/gcp-get-started#set-up-gcp).
+
+Enable APIs:
+- Compute Engine - https://console.cloud.google.com/apis/library/compute.googleapis.com
+- Cloud Resource Manager - https://console.cloud.google.com/apis/library/cloudresourcemanager.googleapis.com
+- IAM - https://console.developers.google.com/apis/library/iam.googleapis.com
+- Kubernetes Engine - https://console.cloud.google.com/apis/library/container.googleapis.com
+- Cloud SQL Admin - https://console.cloud.google.com/apis/library/sqladmin.googleapis.com
+
+Also grant the Terraform service account the roles in the project:
+- roles/editor
+- roles/resourcemanager.projectIamAdmin
+- roles/iam.serviceAccountAdmin
+- roles/compute.instanceAdmin
+
+Also need to annotate the default namespace manually until issue #692 is complete
+https://github.com/hashicorp/terraform-provider-kubernetes/issues/692
+kubectl annotate namespace default cnrm.cloud.google.com/project-id=PROJECT_ID
--- main.tf
+++ main.tf
@@ -1,5 +1,3 @@
-# TODO (jordan) Setup workload identity
-
 variable "project_id" {
   default     = ""
   description = "project ID to use"
@@ -16,6 +14,10 @@ terraform {
       source  = "hashicorp/google-beta"
       version = "3.74.0"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.2.0"
+    }
   }
 
   required_version = ">= 1.0.1"
@@ -29,12 +31,19 @@ provider "google" {
   zone    = "us-east4-c"
 }
 
-data "google_client_config" "this" {}
+data "google_client_config" "current" {}
+
 
 # GKE service account
-resource "google_service_account" "default" {
-  account_id   = "${var.project_name}-gke-sa-id"
-  display_name = "${var.project_name}-gke-sa"
+resource "google_service_account" "gke" {
+  account_id   = "${var.project_name}-gke"
+  display_name = "${var.project_name} GKE node service account"
+}
+
+resource "google_project_iam_member" "gke_gcr" {
+  project = var.project_id
+  role    = "roles/storage.objectViewer"
+  member  = "serviceAccount:${google_service_account.gke.email}"
 }
 
 # GKE cluster
@@ -46,7 +55,7 @@ resource "google_container_cluster" "primary" {
     channel = "RAPID"
   }
   workload_identity_config {
-    identity_namespace = "${data.google_client_config.this.project}.svc.id.goog"
+    identity_namespace = "${var.project_id}.svc.id.goog"
   }
   addons_config {
     config_connector_config {
@@ -63,8 +72,9 @@ resource "google_container_cluster" "primary" {
 
 # Separately Managed Node Pool which allows Terraform to manage it
 resource "google_container_node_pool" "primary_nodes" {
-  name               = "${var.project_name}-gke-node-pool"
-  cluster            = google_container_cluster.primary.name
+  name    = "${var.project_name}-gke-node-pool"
+  cluster = google_container_cluster.primary.name
+
   initial_node_count = 1
   autoscaling {
     min_node_count = 1
@@ -88,10 +98,77 @@ resource "google_container_node_pool" "primary_nodes" {
       enable_secure_boot = true
     }
 
-    service_account = google_service_account.default.email
+    # Works around an issue where Terraform tries to update the node pool when nothing's changed
+    kubelet_config {
+      cpu_manager_policy = "none"
+      cpu_cfs_quota      = null
+    }
+
+    service_account = google_service_account.gke.email
     oauth_scopes = [
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/cloud-platform",
     ]
+  }
+}
+
+
+
+# CNRM Config Connector service account
+resource "google_service_account" "cnrm" {
+  account_id   = "${var.project_name}-cnrm"
+  display_name = "${var.project_name} CNRM Config Connector service account"
+}
+
+resource "google_project_iam_member" "cnrm_editor" {
+  project = var.project_id
+  role    = "roles/editor"
+  member  = "serviceAccount:${google_service_account.cnrm.email}"
+}
+
+resource "google_project_iam_member" "cnrm_iam_admin" {
+  project = var.project_id
+  role    = "roles/iam.serviceAccountAdmin"
+  member  = "serviceAccount:${google_service_account.cnrm.email}"
+}
+
+resource "google_project_iam_member" "cnrm_project_iam_admin" {
+  project = var.project_id
+  role    = "roles/resourcemanager.projectIamAdmin"
+  member  = "serviceAccount:${google_service_account.cnrm.email}"
+}
+
+data "google_iam_policy" "cnrm" {
+  binding {
+    role = "roles/iam.workloadIdentityUser"
+    members = [
+      "serviceAccount:${var.project_id}.svc.id.goog[cnrm-system/cnrm-controller-manager]",
+    ]
+  }
+}
+
+resource "google_service_account_iam_policy" "cnrm" {
+  service_account_id = google_service_account.cnrm.name
+  policy_data        = data.google_iam_policy.cnrm.policy_data
+  depends_on         = [google_container_cluster.primary]
+}
+
+# CNRM Config Connector install
+provider "helm" {
+  kubernetes {
+    host                   = google_container_cluster.primary.endpoint
+    token                  = data.google_client_config.current.access_token
+    client_certificate     = base64decode(google_container_cluster.primary.master_auth.0.client_certificate)
+    client_key             = base64decode(google_container_cluster.primary.master_auth.0.client_key)
+    cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
+  }
+}
+
+resource "helm_release" "cnrm" {
+  name  = "gcp-config-connector"
+  chart = "./charts/cnrm"
+
+  set {
+    name  = "serviceAccount"
+    value = google_service_account.cnrm.email
   }
 }
