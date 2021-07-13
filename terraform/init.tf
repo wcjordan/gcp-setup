a4e7e77fc65aea566e9d2fb8b113c54184b20776
Chris Jordan <flipperkid@gmail.com>
2021-07-12 22:38:53 -0400

    Split up Terraform main file and move to subdirectory (#2)

---
+++ terraform/init.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    google = {
+      # Using beta for config_connector_config
+      source  = "hashicorp/google-beta"
+      version = "3.74.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.2.0"
+    }
+  }
+
+  required_version = ">= 1.0.1"
+}
+
+provider "google" {
+  credentials = file("../service_account_key.json")
+
+  project = var.project_id
+  region  = "us-east4"
+  zone    = "us-east4-c"
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = google_container_cluster.primary.endpoint
+    token                  = data.google_client_config.current.access_token
+    client_certificate     = base64decode(google_container_cluster.primary.master_auth.0.client_certificate)
+    client_key             = base64decode(google_container_cluster.primary.master_auth.0.client_key)
+    cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
+  }
+}
+
+data "google_client_config" "current" {}
---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "project_id" {
+  default     = ""
+  description = "project ID to use"
+}
+
+variable "project_name" {
+  default     = ""
+  description = "project name to use"
+}
