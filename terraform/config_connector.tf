cd290b49f4c29d268770aef9f948a93760826eec
Chris Jordan <chris.jordan@schrodinger.com>
2022-03-28 00:12:33 -0400

    Try forcing a replacement...

--- terraform/config_connector.tf
+++ terraform/config_connector.tf
@@ -42,6 +42,9 @@ resource "helm_release" "cnrm" {
   name  = "gcp-config-connector"
   chart = "charts/cnrm"
 
+  replace      = true
+  force_update = true
+
   set {
     name  = "serviceAccount"
     value = google_service_account.cnrm.email
