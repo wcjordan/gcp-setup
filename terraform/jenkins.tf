7d041034f4842f19feae87d000329966fe5e36d8
Chris Jordan <chris.jordan@schrodinger.com>
2022-05-28 16:41:54 -0400

    Bump Jenkins version

--- terraform/jenkins.tf
+++ terraform/jenkins.tf
@@ -148,7 +148,7 @@ resource "helm_release" "jenkins" {
   name       = "jenkins"
   chart      = "jenkins"
   repository = "https://charts.jenkins.io"
-  version    = "3.11.8"
+  version    = "4.1.1"
 
   # Wait for node pool to exist before installing Jenkins to avoid a timeout
   depends_on = [google_container_node_pool.primary_nodes]
