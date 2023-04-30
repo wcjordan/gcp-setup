7edc516dd634b9e80ad9d432354c76591b1f6179
Chris Jordan <chris.jordan@schrodinger.com>
2023-04-30 11:38:57 -0400

    Provision a Google Artifact Registry to replace GCR and support a Docker builder remote cache

---
+++ terraform/gke_other.tf
@@ -0,0 +1,6 @@
+# Provisioning of misc GKE resources
+resource "google_artifact_registry_repository" "primary" {
+  repository_id = "${var.project_name}-gar"
+  format        = "DOCKER"
+  project       = "${var.project_id}"
+}
