eed3461e7fe87a438d4aaf624f48506c5368f813
Chris Jordan <chris.jordan@schrodinger.com>
2023-11-04 17:13:50 -0400

    Keep the change, but specify the zone explicitly for the node_pool

--- terraform/gke_cluster.tf
+++ terraform/gke_cluster.tf
@@ -1,6 +1,7 @@
 # GKE cluster
 resource "google_container_cluster" "primary" {
   name = "${var.project_name}-gke"
+  location = "${var.gcp_zone}"
 
   enable_shielded_nodes = true
   release_channel {
@@ -32,6 +33,7 @@ resource "google_container_cluster" "primary" {
 resource "google_container_node_pool" "primary_nodes" {
   name    = "${var.project_name}-gke-node-pool"
   cluster = google_container_cluster.primary.name
+  location = "${var.gcp_zone}"
 
   initial_node_count = 1
   autoscaling {
