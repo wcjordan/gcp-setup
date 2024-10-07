25838de6daecf795032d8b124a7089cef377bfe3
Chris Jordan <chris.jordan@schrodinger.com>
2024-10-07 17:54:08 -0400

    Try to minimize changes since the plan seems to drop some things

--- terraform/jenkins.tf
+++ terraform/jenkins.tf
@@ -117,7 +117,7 @@ resource "helm_release" "jenkins" {
   name       = "jenkins"
   chart      = "jenkins"
   repository = "https://charts.jenkins.io"
-  version    = "5.7.3"
+  version    = "5.5.7"
 
   # Wait for node pool to exist before installing Jenkins to avoid a timeout
   depends_on = [google_container_node_pool.primary_nodes]
