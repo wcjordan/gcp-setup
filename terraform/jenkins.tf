7d2582a63b996837de7e9e360e14aa061c6236cf
Chris Jordan <flipperkid@gmail.com>
2026-05-03 13:01:30 -0400

    Bump Jenkins helm chart version

--- terraform/jenkins.tf
+++ terraform/jenkins.tf
@@ -117,7 +117,7 @@ resource "helm_release" "jenkins" {
   name       = "jenkins"
   chart      = "jenkins"
   repository = "https://charts.jenkins.io"
-  version    = "5.8.114"
+  version    = "5.9.18"
   timeout    = 900
 
   # Wait for node pool to exist before installing Jenkins to avoid a timeout
